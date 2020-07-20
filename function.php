abbe92c23874e4bfe7cde36fece09651a426fe66
yaelatif <40276970+yaelatif@users.noreply.github.com>
2020-07-20 08:33:37 +0700

    Update function.php

--- function.php
+++ function.php
@@ -1,22 +1,29 @@
 <?php
-function request($url, $token = null, $data = null, $pin = null){
+
+function request($url, $token = null, $data = null, $pin = null, $otpsetpin = null, $uuid = null){
 
 $header[] = "Host: api.gojekapi.com";
 $header[] = "User-Agent: okhttp/3.10.0";
 $header[] = "Accept: application/json";
-$header[] = "Accept-Language: en-ID";
+$header[] = "Accept-Language: id-ID";
 $header[] = "Content-Type: application/json; charset=UTF-8";
 $header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
-$header[] = "X-User-Locale: en_ID";
-//$header[] = "X-Location: -6.3894201,106.0794195";
-//$header[] = "X-Location-Accuracy: 3.0";
+$header[] = "X-User-Locale: id_ID";
+$header[] = "X-Location: -5.172214,119.465547";
+$header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
     endif;
 if ($token):
 $header[] = "Authorization: Bearer $token";
+endif;
+if ($otpsetpin):
+$header[] = "otp: $otpsetpin";
+endif;
+if ($uuid):
+$header[] = "User-uuid: $uuid";
 endif;
 $c = curl_init("https://api.gojekapi.com".$url);
     curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
@@ -38,12 +45,56 @@ $c = curl_init("https://api.gojekapi.com".$url);
         $body   = substr($response, curl_getinfo($c, CURLINFO_HEADER_SIZE));
     }
     $json = json_decode($body, true);
-    return $json;
+    return $body;
 }
 function save($filename, $content)
 {
-	$save = fopen($filename, "a");
-	fputs($save, "$content\r\n");
-	fclose($save);
+    $save = fopen($filename, "a");
+    fputs($save, "$content\r\n");
+    fclose($save);
+}
+function nama()
+    {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://ninjaname.horseridersupply.com/dutch_name.php");
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $ex = curl_exec($ch);
+    // $rand = json_decode($rnd_get, true);
+    preg_match_all('~(&bull; (.*?)<br/>&bull; )~', $ex, $name);
+    return $name[2][mt_rand(0, 14) ];
+    }
+function getStr($a,$b,$c){
+	$a = @explode($a,$c)[1];
+	return @explode($b,$a)[0];
+}
+function getStr1($a,$b,$c,$d){
+        $a = @explode($a,$c)[$d];
+        return @explode($b,$a)[0];
+}
+function color($color = "default" , $text)
+    {
+        $arrayColor = array(
+            'grey'      => '1;30',
+            'red'       => '1;31',
+            'green'     => '1;32',
+            'yellow'    => '1;33',
+            'blue'      => '1;34',
+            'purple'    => '1;35',
+            'nevy'      => '1;36',
+            'white'     => '1;0',
+        );  
+        return "\033[".$arrayColor[$color]."m".$text."\033[0m";
+    }
+function fetch_value($str,$find_start,$find_end) {
+	$start = @strpos($str,$find_start);
+	if ($start === false) {
+		return "";
+	}
+	$length = strlen($find_start);
+	$end    = strpos(substr($str,$start +$length),$find_end);
+	return trim(substr($str,$start +$length,$end));
 }
 ?>
